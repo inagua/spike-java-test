f3007470430b37d7565d9e739871c1f5df050178
Jacques COUVREUR <jacques@couvreur.me>
2017-02-09 14:49:02 +0100

    closes #5 - Matcher Builder: improve describe method.

--- src/test/java/ch/inagua/spikes/matchers/matchers/IsColleagueBuilderMatcher.java
+++ src/test/java/ch/inagua/spikes/matchers/matchers/IsColleagueBuilderMatcher.java
@@ -1,13 +1,14 @@
 package ch.inagua.spikes.matchers.matchers;
 
-import java.math.BigDecimal;
-
+import ch.inagua.spikes.matchers.models.Colleague;
 import org.apache.commons.lang.StringUtils;
 import org.hamcrest.Description;
 import org.hamcrest.Factory;
 import org.hamcrest.TypeSafeMatcher;
 
-import ch.inagua.spikes.matchers.models.Colleague;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IsColleagueBuilderMatcher extends TypeSafeMatcher<Colleague> {
 
@@ -38,13 +39,15 @@ public class IsColleagueBuilderMatcher extends TypeSafeMatcher<Colleague> {
 	 * toString method for the Expected (values given to the factory above)
 	 */
 	public void describeTo(Description description) {
-		description.appendText("colleague with properties [" //
-				+ "name=" + name //
-				+ ", age=" + age //
-				+ ", service=" + service //
-				+ ", currentProject=" + currentProject //
-				+ ", salary=" + salary //
-				+ "]");
+		List<String> properties = new ArrayList<String>();
+
+		addDescriptionIfDifferent(false, "name=" + name, properties);
+		addDescriptionIfDifferent(false, "age=" + age, properties);
+		addDescriptionIfDifferent(false, "service=" + service, properties);
+		addDescriptionIfDifferent(false, "currentProject=" + currentProject, properties);
+		addDescriptionIfDifferent(false, "salary=" + salary, properties);
+
+		description.appendText("Colleague with properties [" + StringUtils.join(properties, ", ") + "]");
 	}
 
 	/**
@@ -52,13 +55,19 @@ public class IsColleagueBuilderMatcher extends TypeSafeMatcher<Colleague> {
 	 */
 	@Override
 	protected void describeMismatchSafely(Colleague colleague, Description description) {
-		description.appendText("was [" //
-				+ (StringUtils.equals(name, colleague.getName()) ? "" : "name=" + colleague.getName()) //
-				+ (age == colleague.getAge() ? "" : ", age=" + colleague.getAge()) //
-				+ (StringUtils.equals(service, colleague.getService()) ? "" : ", service=" + colleague.getService()) //
-				+ (StringUtils.equals(currentProject, colleague.getCurrentProject()) ? "" : ", currentProject=" + colleague.getCurrentProject()) //
-				+ (areBigDecimalEquals(salary, colleague.getSalary()) ? "" : ", salary=" + colleague.getSalary()) //
-				+ "]");
+		List<String> errors = new ArrayList<String>();
+		addDescriptionIfDifferent(StringUtils.equals(name, colleague.getName()), "name=" + colleague.getName(), errors);
+		addDescriptionIfDifferent(age == colleague.getAge(), "age=" + colleague.getAge(), errors);
+		addDescriptionIfDifferent(StringUtils.equals(service, colleague.getService()), "service=" + colleague.getService(), errors);
+		addDescriptionIfDifferent(StringUtils.equals(currentProject, colleague.getCurrentProject()), "currentProject=" + colleague.getCurrentProject(), errors);
+		addDescriptionIfDifferent(areBigDecimalEquals(salary, colleague.getSalary()), "salary=" + colleague.getSalary(), errors);
+		description.appendText("has [" + StringUtils.join(errors, ", ") + "]");
+	}
+
+	private void addDescriptionIfDifferent(boolean areEqual, String description, List<String> properties) {
+		if (!areEqual) {
+			properties.add(description);
+		}
 	}
 
 	/**
